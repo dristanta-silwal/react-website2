d771b77b2469b9610d6e25110846c5e442d8a541
Dristanta Silwal <you@example.com>
2022-09-24 22:38:21 +0545

    build added

---
+++ build/precache-manifest.519a519d20b8fe28b332e44c25c51b21.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "20f47526a6b9a3cf2ee2",
+    "url": "/react-gh-pages/static/css/main.05223819.chunk.css"
+  },
+  {
+    "revision": "20f47526a6b9a3cf2ee2",
+    "url": "/react-gh-pages/static/js/main.20f47526.chunk.js"
+  },
+  {
+    "revision": "485a7116000ac18e00ff",
+    "url": "/react-gh-pages/static/css/1.c6a1c2d6.chunk.css"
+  },
+  {
+    "revision": "485a7116000ac18e00ff",
+    "url": "/react-gh-pages/static/js/1.485a7116.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "69510e5aa3a9a11e376b8689d7890833",
+    "url": "/react-gh-pages/static/media/logo.69510e5a.png"
+  },
+  {
+    "revision": "e01b110f0dc6dd29b2d56e7a9578330d",
+    "url": "/react-gh-pages/static/media/profile.e01b110f.JPG"
+  },
+  {
+    "revision": "7c64c741362455a0b531c988ae76b063",
+    "url": "/react-gh-pages/index.html"
+  }
+];
