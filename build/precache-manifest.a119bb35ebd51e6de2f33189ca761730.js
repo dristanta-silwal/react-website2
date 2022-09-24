551e566c8b9b2a2d95eb7a41c2976d9979c966f6
Dristanta Silwal <you@example.com>
2022-09-24 22:45:12 +0545

    build added 2

---
+++ build/precache-manifest.a119bb35ebd51e6de2f33189ca761730.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "20f47526a6b9a3cf2ee2",
+    "url": "/static/css/main.05223819.chunk.css"
+  },
+  {
+    "revision": "20f47526a6b9a3cf2ee2",
+    "url": "/static/js/main.20f47526.chunk.js"
+  },
+  {
+    "revision": "485a7116000ac18e00ff",
+    "url": "/static/css/1.c6a1c2d6.chunk.css"
+  },
+  {
+    "revision": "485a7116000ac18e00ff",
+    "url": "/static/js/1.485a7116.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "69510e5aa3a9a11e376b8689d7890833",
+    "url": "/static/media/logo.69510e5a.png"
+  },
+  {
+    "revision": "e01b110f0dc6dd29b2d56e7a9578330d",
+    "url": "/static/media/profile.e01b110f.JPG"
+  },
+  {
+    "revision": "098c97c0d301bfbcd8bc87a4dbbb370c",
+    "url": "/index.html"
+  }
+];
