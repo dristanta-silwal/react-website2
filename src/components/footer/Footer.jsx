d9c84c870b44eda68fe4b32a7211449479cf192c
Dristanta Silwal <dristantasilwal003@gmail.com>
2024-09-13 17:50:32 +0000

    build test 5

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -17,17 +17,17 @@ const Footer = () => {
                         <div className="col-xs-6 col-md-3">
                             <h6>Popular</h6>
                             <ul className="footer-links">
-                                <li><a target={"_blank"} href="https://dristanta-silwal.github.io/TypingGame">Typing Game</a></li>
-                                <li><a target={"_blank"} href="https://dristanta-silwal.github.io/markdown-previewer/">Markdown Previewer</a></li>
-                                <li><a target={"_blank"} href="https://dristanta-silwal.github.io/react-calculator/">Calculator</a></li>
+                                <li><a target={"_blank"} rel={"noreferrer"} href="https://dristanta-silwal.github.io/TypingGame">Typing Game</a></li>
+                                <li><a target={"_blank"} rel={"noreferrer"} href="https://dristanta-silwal.github.io/markdown-previewer/">Markdown Previewer</a></li>
+                                <li><a target={"_blank"} rel={"noreferrer"} href="https://dristanta-silwal.github.io/react-calculator/">Calculator</a></li>
                             </ul>
                         </div>
 
                         <div className="col-xs-6 col-md-3">
                             <h6>Quick Links</h6>
                             <ul className="footer-links">
-                                <li><a href="https://www.facebook.com/dristanta.silwal" target={"_blank"}>Contribute</a></li>
-                                <li><a href="https://dristantasilwal.hashnode.dev/" target={"_blank"}>My blogs</a></li>
+                                <li><a href="https://www.facebook.com/dristanta.silwal" target={"_blank"} rel={"noreferrer"}>Contribute</a></li>
+                                <li><a href="https://dristantasilwal.hashnode.dev/" target={"_blank"} rel={"noreferrer"}>My blogs</a></li>
                             </ul>
                         </div>
                     </div>
@@ -43,11 +43,11 @@ const Footer = () => {
 
                         <div className="col-md-4 col-sm-6 col-xs-12">
                             <ul className="social-icons">
-                                <li><a className="facebook" target={"_blank"} href="https://www.facebook.com/dristanta.silwal"><i className="fa fa-facebook"></i></a></li>
-                                <li><a className="twitter" target={"_blank"} href="https://twitter.com/DristantaSilwal"><i className="fa fa-twitter"></i></a></li>
-                                <li><a className="github" target={"_blank"} href="http://github.com/dristanta-silwal"><i className="fa fa-github"></i></a></li>
-                                <li><a className="linkedin" target={"_blank"} href="https://www.linkedin.com/in/dristantasilwal/"><i className="fa fa-linkedin"></i></a></li>
-                                <li><a className="hashnode" target={"_blank"} href="https://hashnode.com/@Dristanta"><i className="fa fa-hashtag"></i></a></li>
+                                <li><a className="facebook" target={"_blank"} rel={"noreferrer"} href="https://www.facebook.com/dristanta.silwal"><i className="fa fa-facebook"></i></a></li>
+                                <li><a className="twitter" target={"_blank"} rel={"noreferrer"} href="https://twitter.com/DristantaSilwal"><i className="fa fa-twitter"></i></a></li>
+                                <li><a className="github" target={"_blank"} rel={"noreferrer"} href="http://github.com/dristanta-silwal"><i className="fa fa-github"></i></a></li>
+                                <li><a className="linkedin" target={"_blank"} rel={"noreferrer"} href="https://www.linkedin.com/in/dristantasilwal/"><i className="fa fa-linkedin"></i></a></li>
+                                <li><a className="hashnode" target={"_blank"} rel={"noreferrer"} href="https://hashnode.com/@Dristanta"><i className="fa fa-hashtag"></i></a></li>
                             </ul>   
                         </div>
                     </div>  
